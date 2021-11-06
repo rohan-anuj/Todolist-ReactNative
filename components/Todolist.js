4b8a9139b540665a699bda85694f9cab50f69a35
Rohan Tomar <rstomar710@gmail.com>
2021-11-06 21:13:49 +0530

    Todo app completed

--- components/Todolist.js
+++ components/Todolist.js
@@ -1,40 +1,54 @@
-import React, {useState} from "react";
-import { View, Text,TextInput,StyleSheet, FlatList , ScrollView,Button , TouchableOpacity } from  "react-native"
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet, FlatList, ScrollView,Alert, Button, TouchableOpacity } from "react-native"
 
 export default function Todolist() {
-    const [Data,setData]=useState([{name:"ayush",key:3}
-    ,{name:"ayush",key:1},{name:"ayush",key:2}])
+    const [Data, setData] = useState([])
+    const [name, setName] = useState("")
+    
 
-function pressHandled(item){
-    setData(Data.filter(items=>items.key!=item))
-}
+    function pressHandled(item) {
+        setData(Data.filter(items => items.key != item))
+    }
+    function submitHandled() {
+        if(name.length>3){
+        setData(prev => {
+            return [...prev, { name: name, key: Math.random().toString() }]
+        })
+        setName("")
+    }
+    else{
+        Alert.alert("OOps!","Input should be more than 3",[{onPress:()=>console.log("Cancel Pressed")}])
+
+    }
+    }
 
 
     return (
         <View style={styles.container}>
-   <TextInput style={styles.textcontain}  onChangeText={(val)=>console.log(val)} placeholder="Todoo....."/>
-   <View style={{marginTop:10}}><Button  title="Add +" color="orange" />
-            
-         </View> 
+            <TextInput style={styles.textcontain} value={name} onChangeText={(val) => setName(val)} placeholder="Todoo....." />
+            <View style={{ marginTop: 10 }}><Button onPress={submitHandled} title="Add +" color="orange" />
+
+            </View>
             <ScrollView>
-             {Data.map(item=>
-<TouchableOpacity key={item.key} onPress={()=>pressHandled(item.key)} style={{border:"dotted",padding:10, borderWidth:1,borderRadius:10,fontSize:10,borderColor:"orange", fontWeight:"bold", width:300, marginTop:10}}><Text>{item.name}</Text></TouchableOpacity>)}
-</ScrollView>
- </View>
-        );
+                {Data.map(item =>
+                    <TouchableOpacity key={item.key} onPress={() => pressHandled(item.key)} style={{ border: "dotted", padding: 10, borderWidth: 1, borderRadius: 10, fontSize: 10, borderColor: "orange", fontWeight: "bold", width: 300, marginTop: 10 }}>
+                        <Text>{item.name}</Text></TouchableOpacity>)}
+            </ScrollView>
+        </View>
+    );
 }
-const styles=StyleSheet.create({
-    container:{
-        alignItems:"center",
-        marginTop:30
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        marginTop: 30
     },
-    textcontain:{
-        borderBottomWidth:2,
-        borderColor:"orange",
-        padding:5,
-        width:250,
-        fontWeight:"bold",
-        fontSize:20,
-        color:"black"
+    textcontain: {
+        borderBottomWidth: 2,
+        borderColor: "orange",
+        padding: 5,
+        width: 250,
+        fontWeight: "bold",
+        fontSize: 20,
+        color: "black"
     }
 })
